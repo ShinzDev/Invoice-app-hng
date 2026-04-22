ed5848229fe4be7923d1dec82b372466e35cddd7
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2026-04-22 22:59:59 +0100

    feat 002: persist sidebar accross pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { League_Spartan } from "next/font/google";
 import "./globals.css";
+import Sidebar from "./components/Sidebar";
 
 const spartan = League_Spartan({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${spartan.variable} font-sans antialiased bg-[#F8F8FB] text-[#0C0E16] dark:bg-[#141625] dark:text-white transition-colors duration-300`}>
         {children}
+        <Sidebar/>
       </body>
     </html>
   );
