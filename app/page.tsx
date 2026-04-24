eb213511f63e9a13ea3b5e53ab3ef86385e86c4e
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2026-04-24 20:06:51 +0100

    feat 000:upate page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,10 @@
-// app/page.tsx
-import { INVOICES } from "@/lib/invoice-data";
-import EmptyState from "./components/ui/EmptyState";
-import InvoiceItem from "./components/InvoiceItem";
-import Header from "./components/Header"; // Move your header logic here
 
-export default function InvoicesPage() {
-  const isEmpty = INVOICES.length === 0;
+
+export default function Page() {
 
   return (
     <>
-      <Header count={INVOICES.length} />
-      
-      <section className="mt-12">
-        {isEmpty ? (
-          <EmptyState />
-        ) : (
-          <div className="space-y-4">
-            {INVOICES.map((invoice) => (
-              <InvoiceItem key={invoice.id} {...invoice} />
-            ))}
-          </div>
-        )}
-      </section>
+     
     </>
   );
 }
